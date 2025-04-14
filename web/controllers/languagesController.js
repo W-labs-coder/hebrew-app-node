c136c6486dbf5a96b4de2420576f8e804dd12c34
Emmy <emmyconceptng@gmail.com>
2025-04-14 06:38:14 +0100

    language and translation

--- web/controllers/languagesController.js
+++ web/controllers/languagesController.js
@@ -63,7 +63,6 @@ export const addSelectedLanguage = async (req, res) => {
         query: `query {
           shopLocales {
             locale
-            name
             primary
             published
           }
