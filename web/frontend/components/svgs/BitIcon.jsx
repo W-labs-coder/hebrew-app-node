ae1636d9003041222fd215c0c98d16b136f71ca0
Emmy <emmyconceptng@gmail.com>
2025-05-20 15:30:57 +0100

    fixes

--- web/frontend/components/svgs/BitIcon.jsx
+++ web/frontend/components/svgs/BitIcon.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function BitIcon() {
   return (
-   <img src="./bit_pay.png" alt="bit pay" width="100%" />
+    <img
+      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/bit_pay.png"
+      alt="bit pay"
+      width="100%"
+    />
   );
 }
--- web/frontend/components/svgs/IsracardIcon.jsx
+++ web/frontend/components/svgs/IsracardIcon.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function IsracardIcon() {
-  return <img src="./isracard_new.png" width="100%" alt="isracard"/>;
+  return (
+    <img
+      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/isracard_new.png"
+      width="100%"
+      alt="isracard"
+    />
+  );
 }
