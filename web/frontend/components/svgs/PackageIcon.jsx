9bbb4aac09998d70cd0dfd3248a833fa0ba10cbc
Emmy <emmyconceptng@gmail.com>
2025-06-17 08:58:55 +0100

    payment

--- web/frontend/components/svgs/PackageIcon.jsx
+++ web/frontend/components/svgs/PackageIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PackageIcon() {
   return (
     <img
-      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/package-box-svgrepo-com.svg"
+      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/package-delivery-box-13-svgrepo-com.png"
       width="30px"
       alt="package"
     />
--- web/frontend/components/svgs/SendIcon.jsx
+++ web/frontend/components/svgs/SendIcon.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function SendIcon() {
   return (
     <img
-      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/paper-plane-svgrepo-com.svg"
+      src="https://pub-ece2f518b9504c2884b29ab98d7f6283.r2.dev/paper-plane-svgrepo-com.png"
       width="30px"
-      alt="package"
+      alt="paper plane"
     />
   );
 }
