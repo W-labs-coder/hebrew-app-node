3aac1d700a5136b0901c6d48c1fffb9a27dcc185
kuldip korat <kuldipkorat3@gmail.com>
2025-07-19 13:08:05 +0530

    icon

--- web/frontend/components/svgs/SendIcon.jsx
+++ web/frontend/components/svgs/SendIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SendIcon() {
   return (
     <img
-      src="web\frontend\assets\paper-plane.png"
+      src="https://cdn-icons-png.flaticon.com/512/3471/3471653.png"
       width="30px"
       alt="paper plane"
     />
